8a1586fd1d10cad778bf11bbfbdea464a769dada
NhiTT <truong.thi.nhi@sun-asterisk.com>
2026-08-28 17:03:24 +0700

    chore: initialize TripGo Spring Boot project

---
+++ src/main/java/demo/tripgo/TripgoApplication.java
@@ -0,0 +1,13 @@
+package demo.tripgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/tripgo/TripgoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.tripgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
